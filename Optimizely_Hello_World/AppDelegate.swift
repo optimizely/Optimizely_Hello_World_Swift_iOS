422faacc61c2c2c48d66838b2d3a6efe7d43a298
Kody OConnell <kody.oconnell@optimizely.com>
2020-01-10 13:35:55 -0800

    first commit

---
+++ Optimizely_Hello_World/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  Optimizely_Hello_World
+//
+//  Created by Kody O'Connell on 1/10/20.
+//  Copyright © 2020 Kody O'Connell. All rights reserved.
+//
+
+import UIKit
+import Optimizely
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // Build OptimizelyClient
+    let optimizely = OptimizelyClient(sdkKey: "REPLACE_WITH_SDK_KEY", periodicDownloadInterval: 30)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        addListeners()
+        optimizely.start { result in
+            switch result {
+            case .failure(let error):
+             print("Optimizely SDK initiliazation failed: \(error)")
+            case .success:
+             print("Optimizely SDK initialized successfully!")
+            }
+        }
+        return true
+    }
+    
+    
+    func addListeners() {
+        let notificationCenter = optimizely.notificationCenter
+            
+            //notification fired when a value is returned by isFeatureEnabled function call
+        _ = notificationCenter.addDecisionNotificationListener(decisionListener: { (type, userId, attributes, decisionInfo) in
+            print("Received decision notification: \(type) \(userId) \(String(describing: attributes)) \(decisionInfo)")
+        })
+
+            //notification fired when SDK polls for a new datafile and the datafile has changed
+        _ = notificationCenter.addDatafileChangeNotificationListener(datafileListener: { (_) in
+            print("data file did change!")
+        })
+    }
+
+
+
+}
+
---
+++ Optimizely_Hello_World/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Optimizely_Hello_World
+//
+//  Created by Kody O'Connell on 1/10/20.
+//  Copyright © 2020 Kody O'Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  let delegate = UIApplication.shared.delegate as! AppDelegate
+
+    @IBAction func showMessage(sender: UIButton) {
+        let userId = "user123"
+        let attributes: [String: Any] = [
+            "customerId": 123,   // Attributes used for targeted audience-based rollout
+            "isVip": true,
+        ]
+        let enabled = delegate.optimizely.isFeatureEnabled(featureKey: "hello_world", userId: userId, attributes: attributes)
+        print("Feature is enabled? - \(enabled) for userId: \(userId)")
+        
+        let alertController = UIAlertController(title: "Alert", message: enabled ? "Hello World!" : "Nothing to see here...", preferredStyle: UIAlertController.Style.alert)
+        
+        if enabled {
+            alertController.addAction(UIAlertAction(title: "Hello!", style: UIAlertAction.Style.default, handler: nil))
+        } else {
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
